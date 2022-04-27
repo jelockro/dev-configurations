32818d7c4227c4aad2d196133d2f961b1835052a
Parallels <jlockrow@gmail.com>
2022-04-27 00:04:58 -0500

    adds firacode font with glyphs and icons

--- setup.sh
+++ setup.sh
@@ -17,7 +17,8 @@ cd fonts
 ./install.sh
 cd ..
 rm -rf fonts
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
-unzip DroidSansMono.zip -d ~/.fonts
+curl -fLo FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+u
+unzip FiraCode.zip -d ~/.fonts
+unzip FiraCode.zip -d ~/.local/share/fonts
 fc-cache -fv
