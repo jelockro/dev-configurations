96e51d925d78080cb81e272fa676d2180e5a8a1d
Parallels <jlockrow@gmail.com>
2022-04-26 23:16:04 -0500

    adds more plugins

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cp vimrc.bak ~/.vimrc
 sudo apt-get update
-sudo apt-get install -y vim git
+sudo apt-get install -y vim git ctags
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/pack/vendor/start
 git clone --depth 1 \
   https://github.com/preservim/nerdtree.git \
   ~/.vim/pack/vendor/start/nerdtree
 cd ~/.vim/bundle
+sudo pacman -S ctags
 git clone https://github.com/VundleVim/Vundle.vim.git Vundle.vim
 vim +PluginInstall +qall
