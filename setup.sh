e79f2f51ddfe388140a165cecd74c8e1873b54a3
jelockro <jlockrow@gmail.com>
2022-06-27 13:58:33 -0500

    fixes installation on linux

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git Vundle.vim
 vim +PluginInstall +qall
 
 # Install Fonts
+cd ${working_directory}
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
@@ -31,6 +32,23 @@ fc-cache -fv
 cd ${working_directory}
 echo "Sanity Check: What is ZSH_CUSTOM: $ZSH_CUSTOM. \n"
 echo "Where are we: $(pwd) \n" 
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+#git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+echo "source ${ZSH_CUSTOM}/themes/powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# Install tools
+#curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+#python get-pip.py --user
+# Install tools
+#curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+#python get-pip.py --user
+#python -m pip install --user ansible
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+#echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+#install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#chmod +x kubectl
+#mkdir -p ~/.local/bin
+#mv ./kubectl ~/.local/bin/kubectl
+#kubectl version --client
+
